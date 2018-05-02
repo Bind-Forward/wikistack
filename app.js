b38f5a0095db35e48e3d72c0f5a98f892468d4f7
David <davidgcastro93@gmail.com>
2018-05-02 12:25:43 -0400

    minor syntax changes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //require in Express 
 var express = require("express");
 //require in nunjucks templating
-var nunjucks = require('nunjucks')
+var nunjucks = require("nunjucks");
 //initialize an instance of express
 const app = express();
 //require in routes in our routes/index.js file, remember express using this file name as the default
 //makes vars based on the keys in the export object in routes
-const router = require('./routes');
+const router = require("./routes");
 
 const morgan = require("morgan");
-const models = require('./models');
+const models = require("/models");
 const bodyParser = require("body-parser");
 // point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
 // instance, which we'll want to use to add Markdown support later.
@@ -18,35 +18,34 @@ const bodyParser = require("body-parser");
 //NUNJUCKS BOILERPLATE =======================================================
 // point nunjucks to the directory containing templates and turn off caching; 
 //configure returns an Environment instance, which we'll want to use to add Markdown support later.
-const env = nunjucks.configure('views', {
-    noCache: true
+const env = nunjucks.configure("views", {
+	noCache: true
 });
 // have res.render work with html files
-app.set('view engine', 'html');
+app.set("view engine", "html");
 // when res.render works with html files, have it use nunjucks to do so
-app.engine('html', nunjucks.render);
+app.engine("html", nunjucks.render);
 
 //============================================================================
 
 //FOR FORMS
 app.use(bodyParser.urlencoded({
-    extended: true
-}))
-app.use(bodyParser.json())
+	extended: true
+}));
+app.use(bodyParser.json());
 //my Logger
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 //go to routes for all paths
-app.use("/", router)
+app.use("/", router);
 
 //serves public files.
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //ERROR HANDLING, NEEDS 4 ARGS
 //How does it know which is which?
-app.use(function (err, req, res, next) {
-    console.error(err);
-    res.status(500).send(err.message);
-})
+app.use(function (err, req, res ){
+	res.status(500).send(err.message);
+});
 
 //**SYNCING MODELS HERE**
 // **USED DB TO INCLUDE ALL OF OUR TABLES**
@@ -64,14 +63,14 @@ app.use(function (err, req, res, next) {
 
 //Create tables in our DB
 models.db.sync({
-        //Drop all tables and create new ones whenver we make a change to our model. 
-        force: false
-    })
-    .then(function () {
-        console.log('All tables created!');
-        console.log("WIKISTACK DB listening on port 5432")
-        app.listen(3000, function () {
-            console.log('Server is listening on port 3000!');
-        });
-    })
-    .catch(console.error.bind(console));
+	//Drop all tables and create new ones whenver we make a change to our model. 
+	force: false
+})
+	.then(function () {
+		console.log("All tables created!");
+		console.log("WIKISTACK DB listening on port 5432");
+		app.listen(3000, function () {
+			console.log("Server is listening on port 3000!");
+		});
+	})
+	.catch(console.error.bind(console));
